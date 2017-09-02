f771d6798c3d28ff7c7df9e8c5814177ec3f7e37
maxwen <max.weninger@gmail.com>
2017-09-02 12:35:27 +0200

    tinycompress: add back usage of SNDRV_COMPRESS_SET_NEXT_TRACK_PARAM
    kernel headers will trigger usage or not
    
    Change-Id: Ie4de5f52ea06a66204b33ab6232cc3419af8fbd2
    Signed-off-by: RahifM <faizel326@gmail.com>

--- compress.c
+++ compress.c
@@ -572,7 +572,11 @@ int compress_set_gapless_metadata(struct compress *compress,
 	return 0;
 }
 
-#ifdef ENABLE_EXTENDED_COMPRESS_FORMAT
+//<<<<<<< HEAD
+//#ifdef ENABLE_EXTENDED_COMPRESS_FORMAT
+//=======
+#if defined(SNDRV_COMPRESS_SET_NEXT_TRACK_PARAM)
+//>>>>>>> f5e4bfc... tinycompress: add back usage of SNDRV_COMPRESS_SET_NEXT_TRACK_PARAM
 int compress_set_next_track_param(struct compress *compress,
 	union snd_codec_options *codec_options)
 {
@@ -583,6 +587,15 @@ int compress_set_next_track_param(struct compress *compress,
 		return oops(compress, errno, "cannot set next track params\n");
 	return 0;
 }
+//<<<<<<< HEAD
+//=======
+#else
+int compress_set_next_track_param(struct compress *compress __unused,
+    union snd_codec_options *codec_options __unused)
+{
+    return 0;
+}
+//>>>>>>> f5e4bfc... tinycompress: add back usage of SNDRV_COMPRESS_SET_NEXT_TRACK_PARAM
 #endif
 
 bool is_codec_supported(unsigned int card, unsigned int device,
